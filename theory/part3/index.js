e61b44a7629e067162dd759f8b3e6a9bd13f48fd
Ilona Shakurova <ishakuro@student.hive.fi>
2022-08-26 18:07:13 +0300

    Add Part 3.b: Deploying app to internet

--- theory/part3/index.js
+++ theory/part3/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 morgan.token('body', (req, resp) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
