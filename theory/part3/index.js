9fb63a936a99647dcc190c7308506460806fc2ce
Ilona Shakurova <ishakuro@student.hive.fi>
2022-08-26 14:41:52 +0300

    Add request logger

--- theory/part3/index.js
+++ theory/part3/index.js
@@ -22,6 +22,17 @@ let notes = [
 	  important: true
 	}
   ]
+
+  const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
+  }
+  app.use(requestLogger)
+
+
   
   app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
@@ -73,6 +84,12 @@ let notes = [
 	// console.log(request.headers)
 	response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
 
   const PORT = 3001
   app.listen(PORT, () => {
