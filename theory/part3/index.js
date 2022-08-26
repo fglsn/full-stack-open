067bf3070b15d0da0073750a5c7852aee2456821
Ilona Shakurova <ishakuro@student.hive.fi>
2022-08-26 16:10:21 +0300

    Add code to heroku

--- theory/part3/index.js
+++ theory/part3/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require("morgan")
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req, resp) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
 let notes = [
 	{
 	  id: 1,
@@ -91,7 +99,7 @@ const unknownEndpoint = (request, response) => {
   
   app.use(unknownEndpoint)
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
   })
