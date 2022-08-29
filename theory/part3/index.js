93a98911c8030df4ee9a3ac90bb2143bd4575ee1
Ilona Shakurova <leverseau19@gmail.com>
2022-08-29 13:03:32 +0300

    uibuild

--- theory/part3/index.js
+++ theory/part3/index.js
@@ -13,7 +13,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 let notes = [
 	{
 	  id: 1,
-	  content: "HTML is easy",
+	  content: "HTML",
 	  date: "2022-05-30T17:30:31.098Z",
 	  important: true
 	},
