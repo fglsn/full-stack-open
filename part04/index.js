2c5dfbf080b96a21d8fc3742a3dc8ef3dac881f6
Ilona Shakurova <ishakuro@student.hive.fi>
2022-09-08 11:38:25 +0300

    Refactor blogsApp, add structure

--- part04/index.js
+++ part04/index.js
@@ -1,68 +1,10 @@
-require('dotenv').config()
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const { response } = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
+const server = http.createServer(app)
 
-blogSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString()
-		delete returnedObject._id
-		delete returnedObject.__v
-	}
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-
-mongoose.connect(mongoUrl)
-	.then(result => {
-		console.log('connected to MongoDB')
-	})
-	.catch((error) => {
-		console.log('error connecting to MongoDB:', error.message)
-	})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-	Blog
-	.find({})
-	.then(blogs => {
-		response.json(blogs)
-	})
-})
-
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`)
 })
