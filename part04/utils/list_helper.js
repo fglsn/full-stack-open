35ee1aa89e682fe3f2f38500984c8ce312c9f3f3
Ilona Shakurova <ishakuro@student.hive.fi>
2022-09-08 15:01:05 +0300

    Add 4.5*: helper functions and unit tests, step3

--- part04/utils/list_helper.js
+++ part04/utils/list_helper.js
@@ -11,16 +11,39 @@ const totalLikes = (blogs) => {
 	else if (blogsCount === 1)
 		return blogs[0].likes
 	else {
-		const total = blogs.reduce((sum, obj) => {
-			return sum + obj.likes
+		const total = blogs.reduce((sum, blog) => {
+			return sum + blog.likes
 		}, 0)
 		console.log(total)
 		return (total)
-		
+
 	}
+}
+
+const favouriteBlog = (blogs) => {
+
+	if (blogs.length === 0)
+		return NaN
+	else {
+		const maxLikesValue = Math.max(...blogs.map(blog => blog.likes))
+
+		const topBlog = blogs.filter(obj => {
+			return obj.likes === maxLikesValue
+		})
+
+		const result = {
+			title: topBlog[0].title,
+			author: topBlog[0].author,
+			likes: topBlog[0].likes
+		}
+
+		return result
+	}
+
 }
 
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favouriteBlog
 }
