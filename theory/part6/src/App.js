4f82882352517b907118e6a8a76d4f20388ab2f2
Ilona Shakurova <ishakuro@student.hive.fi>
2022-09-26 14:01:31 +0300

    Add redux-toolkit theory part / notesApp

--- theory/part6/src/App.js
+++ theory/part6/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-
 	return (
-	  <div>
-		<NewNote />
-		<Notes />
-	  </div>
+		<div>
+			<NewNote />
+			<VisibilityFilter />
+			<Notes />
+		</div>
 	)
-  }
+}
 
 export default App
