ab883f25fdacab48cb7b37f87c5d60a8c9097860
Ilona Shakurova <ishakuro@student.hive.fi>
2022-09-27 10:55:39 +0300

    Add study materials / Asynchronous actions and redux thunk

--- theory/part6/src/App.js
+++ theory/part6/src/App.js
@@ -4,15 +4,13 @@ import Notes from './components/Notes'
 import NewNote from './components/NewNote'
 import VisibilityFilter from './components/VisibilityFilter'
 
-import noteService from './services/notes'
-import { setNotes } from './reducers/noteReducer'
+import { initializeNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
 	const dispatch = useDispatch()
 	useEffect(() => {
-		noteService
-			.getAll().then(notes => dispatch(setNotes(notes)))
+		dispatch(initializeNotes())
 	}, [dispatch])
 
 	return (
--- theory/part6/src/index.js
+++ theory/part6/src/index.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 import App from './App'
-
-import filterReducer from './reducers/filterReducer'
-
-import noteService from './services/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
-
-const store = configureStore({
-	reducer: {
-		notes: noteReducer,
-		filter: filterReducer
-	}
-})
-
-// console.log(store.getState())
-
-noteService.getAll().then(notes =>
-	store.dispatch(setNotes(notes))
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
