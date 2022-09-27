d63861aee26d2c9c0acd0337382084b3b5c11f11
Ilona Shakurova <ishakuro@student.hive.fi>
2022-09-27 13:45:01 +0300

    Add 6.15-6.16  Anecdotes and the backend step3-4 / Modify the initialization of Redux store and creation of a new anecdote

--- part06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,31 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
 	name: 'anecdotes',
 	initialState: [],
 	reducers: {
-		createAnecdote(state, action) {
-			state.push(action.payload)
-		},
-		vote(state, action) {
-			const id = action.payload
-			const voteFor = state.find(anecdote => anecdote.id === id)
-			const changedAnecdote = {
-				...voteFor,
-				votes: voteFor.votes + 1
-			}
-			const updatedList = state.map(anecdote =>
-				anecdote.id !== id ? anecdote : changedAnecdote)
-			return updatedList.sort((a, b) => b.votes - a.votes)
-		},
 		appendAnecdote(state, action) {
 			state.push(action.payload)
 		},
 		setAnecdotes(state, action) {
-			return action.payload
+			return (action.payload).sort((a, b) => b.votes - a.votes)
 		}
 	}
 })
 
-export const { createAnecdote, vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+	return async dispatch => {
+		const anecdotes = await anecdoteService.getAll()
+		dispatch(setAnecdotes(anecdotes))
+	}
+}
+
+export const createAnecdote = content => {
+	return async dispatch => {
+		const newAnecdote = await anecdoteService.createNew(content)
+		dispatch(appendAnecdote(newAnecdote))
+	}
+}
+
+export const vote = (anecdotes, id) => {
+	const voteFor = anecdotes.find(anecdote => anecdote.id === id)
+	const changedAnecdote = {
+		...voteFor,
+		votes: voteFor.votes + 1
+	}
+	return async dispatch => {
+		await anecdoteService.addVote(id, changedAnecdote)
+		const updatedList = anecdotes
+			.map(anecdote =>
+				anecdote.id !== id ? anecdote : changedAnecdote)
+		dispatch(setAnecdotes(updatedList))
+	}
+}
+
 export default anecdoteSlice.reducer
