414b06496d9cf85711d4761228f9d6063afc1b28
Ilona Shakurova <ishakuro@student.hive.fi>
2022-10-24 17:59:39 +0300

    Fix format

--- part09/patientor-front/src/AddPatientModal/index.tsx
+++ part09/patientor-front/src/AddPatientModal/index.tsx
@@ -4,21 +4,21 @@ import { Alert } from "@material-ui/lab";
 import AddPatientForm, { PatientFormValues } from "./AddPatientForm";
 
 interface Props {
-  modalOpen: boolean;
-  onClose: () => void;
-  onSubmit: (values: PatientFormValues) => void;
-  error?: string;
+	modalOpen: boolean;
+	onClose: () => void;
+	onSubmit: (values: PatientFormValues) => void;
+	error?: string;
 }
 
 const AddPatientModal = ({ modalOpen, onClose, onSubmit, error }: Props) => (
-  <Dialog fullWidth={true} open={modalOpen} onClose={() => onClose()}>
-    <DialogTitle>Add a new patient</DialogTitle>
-    <Divider />
-    <DialogContent>
-      {error && <Alert severity="error">{`Error: ${error}`}</Alert>}
-      <AddPatientForm onSubmit={onSubmit} onCancel={onClose} />
-    </DialogContent>
-  </Dialog>
+	<Dialog fullWidth={true} open={modalOpen} onClose={() => onClose()}>
+		<DialogTitle>Add a new patient</DialogTitle>
+		<Divider />
+		<DialogContent>
+			{error && <Alert severity="error">{`Error: ${error}`}</Alert>}
+			<AddPatientForm onSubmit={onSubmit} onCancel={onClose} />
+		</DialogContent>
+	</Dialog>
 );
 
 export default AddPatientModal;
